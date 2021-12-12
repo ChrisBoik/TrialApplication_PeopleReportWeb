7cbcd567d71984b0a035a4abeb6afa1f2461508a
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:20:46 +0200

    Add required component references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http"
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 // import { OrderCreateComponent } from "./components/order/order-create/order-create.component"
 
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './router/app-routing.routings';
-import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { PostListComponent } from './components/post/post-list/post-list.component';
-import { ViewPostComponent } from './components/post/view-post/view-post.component';
 
 //NgBootstrap dependency for toolsets
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PostCreateComponent } from './components/post/post-create/post-create.component';
-import { ErrorInterceptor } from './interceptors/error.interceptor';
+
 import { MatDialogModule } from '@angular/material/dialog';
-import { EditPostComponent } from './components/post/edit-post/edit-post.component';
-import { PermissionComponent } from './components/permission/permission.component';
+import { EditPersonComponent } from './pages/post/edit-person/edit-person.component';
+import { CreatePersonComponent } from './pages/post/create-person/create-person.component';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { CommonModule } from '@angular/common';
+import { PeopleListComponent } from './pages/post/list-people/people-list.component';
+import { BirthdayReportComponent } from './pages/post/birthday-report/birthday-report.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    // OrderCreateComponent,
-    ViewPostComponent,
-    PostListComponent,
-    LoginComponent,
-    RegisterComponent,
-    PostCreateComponent,
-    EditPostComponent,
-    PermissionComponent
+    PeopleListComponent,
+    CreatePersonComponent,
+    EditPersonComponent,
+    BirthdayReportComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -48,9 +43,10 @@ import { PermissionComponent } from './components/permission/permission.componen
 
     BrowserAnimationsModule,
     HttpClientModule,
+    CommonModule,
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
