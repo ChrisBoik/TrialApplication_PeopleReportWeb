7b4c50ab837965ad85b6116081760bc72d272b90
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:05:55 +0200

    Remove unused, generated, and placeholder content

--- src/app/models/post.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Post {
-    id?: string;
-    title: string;
-    timestamp?: string;
-    username: string;
-    department: string;
-    text: string;
-}
--- src/app/services/auth-data.model.ts
+++
@@ -1,4 +0,0 @@
-export interface AuthData{
-    email: String,
-    password: String
-}
