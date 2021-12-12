69af939b5a92b11a21efba1604ab5a520689b936
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:20:58 +0200

    Add page routing

--- src/app/router/app-routing.routings.ts
+++ src/app/router/app-routing.routings.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from '../services/router-services/auth-guard.service';
-import { LoginComponent } from '../components/auth/login/login.component';
-import { RegisterComponent } from '../components/auth/register/register.component';
-import { PostListComponent } from '../components/post/post-list/post-list.component';
-import { ViewPostComponent } from '../components/post/view-post/view-post.component';
-import { PostCreateComponent } from '../components/post/post-create/post-create.component';
-import { EditPostComponent } from '../components/post/edit-post/edit-post.component';
-import { PermissionComponent } from '../components/permission/permission.component';
+import { BirthdayReportComponent } from '../pages/post/birthday-report/birthday-report.component';
+import { CreatePersonComponent } from '../pages/post/create-person/create-person.component';
+import { EditPersonComponent } from '../pages/post/edit-person/edit-person.component';
+import { PeopleListComponent } from '../pages/post/list-people/people-list.component';
 // import { OrderCreateComponent } from '../components/order/order-create/order-create.component';
 // import { OrderPlacedComponent } from '../components/order/order-placed/order-placed.component';
 
 
 const routes: Routes = [
-  { path: "", component: PostListComponent, canActivate: [AuthGuardService] },
-  { path: "post/create", component: PostCreateComponent, canActivate: [AuthGuardService] },
-  { path: "post/edit/:id", component: EditPostComponent, canActivate: [AuthGuardService] },
-  { path: "post/:id", component: ViewPostComponent, canActivate: [AuthGuardService] },
-  { path: "users", component: PermissionComponent, canActivate: [AuthGuardService] },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "", component: PeopleListComponent, },
+  { path: "person/new", component: CreatePersonComponent,  },
+  { path: "person/edit/:id", component: EditPersonComponent, },
+
+  { path: "birthdays", component: BirthdayReportComponent, },
 ];
 
 @NgModule({
