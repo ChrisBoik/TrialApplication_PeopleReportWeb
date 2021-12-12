75e8b930dc37f7624126ac971230942d06bdd75a
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:19:22 +0200

    Add create person page component

---
+++ src/app/pages/post/create-person/create-person.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms"
+import { Person } from "src/app/models/person.model"
+import { Router } from '@angular/router';
+import { PeopleService } from 'src/app/services/people-service.service';
+
+@Component({
+  selector: 'app-order-create',
+  templateUrl: './create-person.component.html',
+  styleUrls: ['./create-person.component.css']
+})
+export class CreatePersonComponent implements OnInit {
+
+  constructor(public peopleService: PeopleService, public router: Router) { }
+  submitted = false;
+
+  ngOnInit(): void {
+  }
+
+  // usernameError = "Username error"
+  // emailError = "Email error"
+  // orderError = "Order error"
+
+  onCreatePerson(PostForm: NgForm): void {
+    this.submitted = true;
+    if (PostForm.invalid) {
+      return;
+    }
+    const person: Person = new Person({
+      Id: null,
+      First_Name: PostForm.value.first_name,
+      Last_Name: PostForm.value.last_name,
+      Date_of_Birth: PostForm.value.date_of_birth,
+    });
+
+    // Redirect to people list
+    this.peopleService.addPerson(person).then(newPostId => this.router.navigate([`/`])).catch(err => console.error("Error creating person, try again."));
+  }
+
+}
